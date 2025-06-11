4e3ff84db71c4c25ef4c33672ca1ad0f5b677eb4
Nathan Yearout <nathan@payload.com>
2025-06-11 10:20:32 -0400

    Neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,12 +1,8 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        position = "right",
-        width = 30,
-      },
-      auto_expand_width = false,
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "right",
     },
   },
 }
