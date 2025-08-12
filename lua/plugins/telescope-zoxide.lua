aa9f28f49413f491a596f2baf8c8f94df229e783
Nathan Yearout <nathan@payload.com>
2025-08-12 09:34:59 -0400

    Add zoxide hotkeys

--- lua/plugins/telescope-zoxide.lua
+++ lua/plugins/telescope-zoxide.lua
@@ -1,7 +1,18 @@
-{
-  "jvgrootveld/telescope-zoxide",
-  dependencies = { "nvim-telescope/telescope.nvim" },
-  config = function()
-    require("telescope").load_extension("zoxide")
-  end,
+return {
+  {
+    "jvgrootveld/telescope-zoxide",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    keys = {
+      {
+        "<leader>fz",
+        function()
+          require("telescope").extensions.zoxide.list()
+        end,
+        desc = "Find directory (zoxide)",
+      },
+    },
+    config = function()
+      require("telescope").load_extension("zoxide")
+    end,
+  },
 }
